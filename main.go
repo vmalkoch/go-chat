aae01d0d874901928371240394ac642bcc1a0b8b
vmalkoch <vmalkoch@gmail.com>
2017-10-26 11:37:19 +0300

    public folder

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"html/template"
 	"net/http"
+
+	"./data"
 )
 
 func main() {
@@ -34,13 +36,25 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	files := []string{"templates/layout.html",
-		"templates/navbar.html",
-		"templates/index.html"}
 
-	templates := template.Must(template.ParseFiles(files...))
 	threads, err := data.Threads()
-	if err == null {
+	if err == nil {
+		_, err := session(w, r)
+		public_tmpl_files := []string{"templates/layout.html",
+			"templates/public.navbar.html",
+			"templates/index.html"}
+		private_tmpl_files := []string{"templates/layout.html",
+			"templates/private.navbar.html",
+			"templates/index.html"}
+
+		var templates *template.Template
+
+		if err != nil {
+			templates = template.Must(template.ParseFiles(private_tmpl_files...))
+		} else {
+			templates = template.Must(template.ParseFiles(public_tmpl_files...))
+		}
+
 		templates.ExecuteTemplate(w, "layout", threads)
 	}
 }
